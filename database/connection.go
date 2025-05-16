c4ce0bcd96a30f04a63de6117e97513fa84c036b
nesyasal <nesya.sr@gmail.com>
2025-05-16 22:33:47 +0700

    feat: add connection database

---
+++ database/connection.go
@@ -0,0 +1,27 @@
+package database
+
+import (
+    "context"
+    "log"
+    "time"
+
+    "go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var MongoClient *mongo.Client
+var DB *mongo.Database
+
+func Connect() {
+    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+    defer cancel()
+
+    client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+    if err != nil {
+        log.Fatal("Gagal koneksi MongoDB:", err)
+    }
+
+    MongoClient = client
+    DB = client.Database("todo_db")
+    log.Println("✅ Terkoneksi ke MongoDB")
+}
