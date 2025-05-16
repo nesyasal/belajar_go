06abb968c45997740c15b0f672aaa0f65499ca29
nesyasal <nesya.sr@gmail.com>
2025-05-16 22:30:34 +0700

    feat: add main.go

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "todo-app/database"
+    "todo-app/routes"
+
+    "github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func main() {
+
+	app := fiber.New()
+    database.Connect()
+	app.Use(logger.New(logger.Config{
+        Format: "${status} - ${method} ${path}\n",
+    }))
+    routes.SetupRoutes(app)
+
+    app.Listen(":3000")
+}
