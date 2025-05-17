b69228d57dd3fd002d00b6a368d91d2c61cd0afb
nesyasal <nesya.sr@gmail.com>
2025-05-17 23:00:05 +0700

    add routes get all users

--- routes/auth.go
+++ routes/auth.go
@@ -1,15 +1,18 @@
-
 package routes
 
 import (
-    "todo-app/controllers"
+	"todo-app/controllers"
 
-    "github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2"
 )
 
 func AuthRoutes(app *fiber.App) {
-    api := app.Group("/auth")
-    api.Post("/register", controllers.Register)
-    api.Post("/login", controllers.Login)
+	api := app.Group("/auth")
+	api.Post("/register", controllers.Register)
+	api.Post("/login", controllers.Login)
 }
 
+func SetupRoutes(app *fiber.App) {
+	api := app.Group("/api")
+	api.Get("/users-list", controllers.GetAllUsers)
+}
