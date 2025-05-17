9142c083e4ee2e050b7554296e61c228614dec75
nesyasal <nesya.sr@gmail.com>
2025-05-17 22:37:04 +0700

    perbaikan struct

--- models/user.go
+++ models/user.go
@@ -1,8 +1,10 @@
 package models
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type User struct {
-    ID       string `json:"id" bson:"_id,omitempty"`
-    Name     string `json:"name" bson:"name"`
-    Email    string `json:"email" bson:"email"`
-    Password string `json:"password" bson:"password"`
+	ID       primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Name     string             `json:"name" bson:"name"`
+	Email    string             `json:"email" bson:"email"`
+	Password string             `json:"password" bson:"password"`
 }
