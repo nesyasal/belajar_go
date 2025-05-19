5b8f8c3d6edad1f7ab9c6da9f0b7b878cc6a2c04
nesyasal <nesya.sr@gmail.com>
2025-05-19 22:35:42 +0700

    update

--- controllers/auth_controller.go
+++ controllers/auth_controller.go
@@ -60,29 +60,4 @@ func Login(c *fiber.Ctx) error {
 	})
 }
 
-func GetAllUsers(c *fiber.Ctx) error {
-	collection := database.DB.Collection("users")
 
-	// Ambil semua dokumen
-	cursor, err := collection.Find(context.Background(), bson.M{}, options.Find())
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Gagal mengambil data pengguna"})
-	}
-	defer cursor.Close(context.Background())
-
-	var users []models.UserResponse
-	for cursor.Next(context.Background()) {
-		var user models.User
-		if err := cursor.Decode(&user); err != nil {
-			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Gagal mendekode data pengguna"})
-		}
-
-		users = append(users, models.UserResponse{
-			ID:    user.ID.Hex(),
-			Name:  user.Name,
-			Email: user.Email,
-		})
-	}
-
-	return c.JSON(users)
-}
